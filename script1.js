4a659f4cb3325e31dde9b598267b6b728c03842b
Git-Hub123-sys78 <gunatejaswi.23bce8937@vitapstudent.ac.in>
2025-06-03 18:00:13 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,7 @@
+let num=window.prompt("enter a number");
+if(num>=0){
+    console.log("the number is positive");
+}
+else{
+    console.log("the number is negative");
+}
---
+++ script3.js
@@ -0,0 +1,7 @@
+let num=prompt("enter the number how many times you want to repeat the text");
+let text=prompt("enter the text");
+let result=" ";
+for(let i=0;i<num;i++){
+    result=result+text+"\n";
+}
+console.log(result);
